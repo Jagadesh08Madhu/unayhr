4c4b5d7afdbfa33e40a630bf2326ca9ef6b63a58
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2025-03-17 16:06:21 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ export default function Home() {
     <section className='font-lora py-20 bg-[radial-gradient(circle_at_top_left,_rgba(47,146,211,0.3)_110%,_rgba(36,72,116,0.9)_0%)]'>
       <div className='flex flex-col lg:flex-row justify-center items-center mx-5 lg:mx-20 gap-5'>
         <div className='flex flex-col lg:w-1/2 justify-start items-start gap-5'>
-          <h1 className='text-2xl font-semibold'>
+          <h1 className='text-lg lg:text-2xl font-semibold'>
             HR Compliance for <span className='text-[#2f92d3]'> UKVI Sponsorship</span>
           </h1>
-          <h1 className='text-xl font-medium'>
+          <h1 className='text-lg lg:text-xl font-medium'>
             Empower Your Workforce: Professional HR & Payroll Solutions for Every Stage of the Employee Lifecycle
           </h1>
           <ul className='flex flex-col gap-4'>
