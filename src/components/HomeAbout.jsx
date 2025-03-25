2e85854479356a1c4d713a011732739847a1cd6d
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2025-03-25 14:45:36 +0530

    chnages

---
+++ src/components/HomeAbout.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import about from '../assets/about.png'
+import { VscActivateBreakpoints } from "react-icons/vsc";
+
+export default function HomeAbout() {
+  return (
+    <section className='py-20 font-lora'>
+      <div className='flex gap-5'>
+        {/* Image section */}
+        <div className='lg:w-1/2'>
+            <img src={about} alt="" />
+        </div>
+        {/* Content section */}
+        <div className='lg:w-1/2 flex flex-col gap-5'>
+            <h1 className='text-2xl lg:text-4xl'>Empower your workflow with <span className='text-[#2f92d3]'>UnayHR Payroll</span></h1>
+
+            <ul className='flex flex-col gap-3'>
+                    
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span>UnayHR Payroll manage employees salaries, wages, bonuses</li>
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span> Runs Unlimited Payslips effectively</li>
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span>Manages employee Time management</li>
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span>Manages employee Holiday management</li>
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span>Manage Employee Task</li>
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span>Attendance Management</li>
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span>Dedicated Staff Private Panel for Payslip Privacy</li>
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span>Customized Payslip Design</li>
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span>Print Payslip Multilanguage (16 Language)</li>
+                <li className='flex items-center gap-2'><span className='text-[#2f92d3] text-xl'><VscActivateBreakpoints /></span>Unlimited Dedicated Training and support</li>
+            </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import hero from '../assets/hero.png';
 import { GiCheckMark } from "react-icons/gi";
 import './home.css'
+import { div } from 'framer-motion/client';
+import HomeAbout from '../components/HomeAbout';
 
 export default function Home() {
   return (
-    <section className='font-lora py-20  bg-gradient-to-tr from-[#4b98cb] to-white text'>
+    <div>
+      <section className='font-lora py-20  bg-gradient-to-tr from-[#4b98cb] to-white text'>
       <div className='flex flex-col lg:flex-row justify-center items-center mx-5 lg:mx-20 gap-5'>
         <div className='flex flex-col lg:w-1/2 justify-start items-start gap-5 order-2 lg:order-1'>
           <h1 className='text-lg lg:text-2xl font-semibold'>
@@ -41,5 +44,7 @@ export default function Home() {
         </div>
       </div>
     </section>
+    <HomeAbout/>
+    </div>
   );
 }
