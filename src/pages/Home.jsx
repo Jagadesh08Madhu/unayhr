9333809f4efddf51c642fd3f72a7b192140f7617
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2025-04-03 14:45:09 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import Whychoose from '../components/Whychoose';
 import Homeservices from '../components/Homeservices';
 import HomeIndustry from '../components/HomeIndustry';
 import LastAbout from '../components/LastAbout';
+import Blog from '../components/Blog';
 
 export default function Home() {
 
@@ -78,6 +79,7 @@ export default function Home() {
       <Homeservices/>
       <LastAbout/>
       <HomeIndustry/>
+      <Blog/> 
     </div>
   );
 }
